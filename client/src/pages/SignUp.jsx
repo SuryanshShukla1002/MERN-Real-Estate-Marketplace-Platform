bd83abe3e6a386ea342f0a308d34de95d25f34bb
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-05 15:45:16 +0530

    creating the error if same username exist 2 times

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,44 @@
-import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link , useNavigate} from "react-router-dom";
 export default function SignUp() {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
   const handleChange = (e) => {
-     setFormData({
-      ...formData, 
-      [e.target.id]: e.target.value
-     })
-  }
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
   const hadleSubmit = async (e) => {
-    e.preventDefault()
-    const res = await fetch('/api/auth/signup', formData)
-  }
-  console.log(formData);
-  
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null) // if everthing is fine then we clear the error 
+      navigate('')
+      console.log(data);
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+  };
+  // console.log(formData);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -39,8 +64,11 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign-up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -49,6 +77,7 @@ export default function SignUp() {
           <span className="text-blue-500 hover:text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p> }
     </div>
   );
 }
