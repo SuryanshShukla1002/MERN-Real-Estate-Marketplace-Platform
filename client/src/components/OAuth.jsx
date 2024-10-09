60578585826714f3c9eaf454dd8a42c46983a1c6
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-09 16:52:29 +0530

    Add Google auth API call with user data and dispatch sign-in success

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,10 @@
 import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/User/userSlice';
 
 export default function OAuth() {
+  const dispatch = useDispatch()
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider()
@@ -9,8 +12,16 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth, provider)
       // console.log(result);
-      
-      
+
+      const res = await fetch('/api/auth/google' , {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: result.user.displayName , email: result.user.email , photo: result.user.photoURL})
+      })
+      const data = await res.json()  
+      dispatch(signInSuccess(data))
     } catch (error) {
       console.log("could not sign in whith google", error);
     }
