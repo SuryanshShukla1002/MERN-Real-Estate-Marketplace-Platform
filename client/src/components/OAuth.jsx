2384a847ba6a0d666435e449b982bb599a8f0944
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-15 00:14:03 +0530

    signinSucces navigate to home

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,27 +1,36 @@
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
-import { app } from '../firebase';
-import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../redux/User/userSlice';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/User/userSlice";
+import {useNavigate} from 'react-router-dom'
 
 export default function OAuth() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
   const handleGoogleClick = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-      const result = await signInWithPopup(auth, provider)
+      const result = await signInWithPopup(auth, provider);
       // console.log(result);
-
-      const res = await fetch('/api/auth/google' , {
-        method: 'POST',
+      // console.log(result.user.photoURL);
+      
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({name: result.user.displayName , email: result.user.email , photo: result.user.photoURL})
-      })
-      const data = await res.json()  
-      dispatch(signInSuccess(data))
+        credentials: 'include',
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/')
     } catch (error) {
       console.log("could not sign in whith google", error);
     }
