db17329abe2dba1739995e5a74f60538a85559ed
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-17 23:11:08 +0530

    uneeded console.log removed

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -40,12 +40,13 @@ export const google = async (req, res, next) => {
         } else {
             const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedpassword = bcryptjs.hashSync(generatePassword, 10);
-            const newUser = new User({ username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedpassword, avatar: req.body.photo, 
+            const newUser = new User({
+                username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedpassword, avatar: req.body.photo,
             });
             await newUser.save();
-            console.log(req.body.photo);
-            console.log(newUser);
-            
+            // console.log(req.body.photo);
+            // console.log(newUser);
+
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = newUser._doc;
             res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
