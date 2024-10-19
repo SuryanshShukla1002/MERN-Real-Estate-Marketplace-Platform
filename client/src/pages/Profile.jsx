363064353927796eb1b0c47101ced68fa91b1986
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-19 15:43:54 +0530

    Handle file upload errors, set avatar URL after successful upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,56 @@
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
-  console.log(file);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [filePercentage, setFilePercentage] = useState(0);
+  const [formData, setFormData] = useState({});
+  // console.log(file);
+  console.log(formData);
+  console.log(filePercentage);
+  console.log(fileUploadError);
 
   useEffect(() => {
-    if(file) {
-      handleFileUpload();
+    if (file) {
+      handleFileUpload(file);
     }
-  } , [file])
-  
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name; // for uniqoue file name
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePercentage(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    ); // we wanna get the file so for that
+  };
 
   return (
-    <div className="p-3 max-w-lg">
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
         <input
