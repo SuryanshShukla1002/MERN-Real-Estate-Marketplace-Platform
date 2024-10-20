a058404f2913730eb3df0819f609c0298889095b
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-20 09:34:28 +0530

    Add cookie-parser middleware

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDb!");
@@ -15,22 +16,23 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error'
+    const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
         success: false,
         statusCode,
         message
-    })
-})
+    });
+});
