d5e80ddb3f701e9dee107b18227d2b7abfdd2fed
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-22 04:19:36 +0530

    Refactor form change handler to dynamically update formData state

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -49,6 +49,10 @@ export default function Profile() {
     ); // we wanna get the file so for that
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value }); // extract the changes ad put it inside here formdata
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -62,17 +66,19 @@ export default function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar} // for displaying the image
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-        <p>
+        <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className="text-red-700">Error Image upload</span>
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2mb)
+            </span>
           ) : filePercentage > 0 && filePercentage < 100 ? (
             <span className="text-slate-700">{`Uploading ${filePercentage}%`}</span>
           ) : filePercentage === 100 ? (
-            <span>Image succesfully uploaded</span>
+            <span className="text-green-700">Image succesfully uploaded</span>
           ) : (
             ""
           )}
@@ -81,19 +87,24 @@ export default function Profile() {
           type="text"
           id="username"
           placeholder="username"
+          defaultValue={currentUser.username}
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           id="email"
           placeholder="email"
+          defaultValue={currentUser.email}
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="text"
           id="passsword"
           placeholder="passsword"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           update
