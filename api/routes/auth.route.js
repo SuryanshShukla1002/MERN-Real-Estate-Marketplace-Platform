3714c4599ac6b3c8aca8d0044ee23ae3a4900032
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-24 15:28:05 +0530

    Added sign-out route for user logou

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { google, signin, signup } from '../controllers/auth.controler.js';
+import { google, signin, signOut, signup } from '../controllers/auth.controler.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post('/google', google);
+router.get('/signout' , signOut )
 
 export default router;
