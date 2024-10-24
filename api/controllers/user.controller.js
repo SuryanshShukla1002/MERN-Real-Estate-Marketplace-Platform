41ee8174dd68397e3cc8454dbf4fc4991c674b72
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-24 15:22:35 +0530

    add delete user method

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,13 +15,27 @@ export const updateUser = async (req, res, next) => {
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            // to update the properties we want and only update what we want
             $set: {
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password,
                 avatar: req.body.avatar
             }
-        });
+        }, { new: true });
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id != req.params.id) return next(errorHandler(401), "You can only delete your own account");
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User has been deleted');
     } catch (error) {
         next(error);
     }
