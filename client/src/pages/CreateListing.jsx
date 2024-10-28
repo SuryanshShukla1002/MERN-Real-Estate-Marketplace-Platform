3b46caf5a7193e673d91917d1d432e4d33783ae9
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-28 15:45:00 +0530

    check boxs created

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -29,6 +29,28 @@ export default function CreateListing() {
             id="address"
             required
           />
+          <div className='flex gap-6 flex-wrap'>
+          <div className=" flex gap-2 ">
+            <input type="checkbox" id="sale" className="w-5" />
+            <span>Sell</span>
+          </div>
+          <div className=" flex gap-2 ">
+            <input type="checkbox" id="rent" className="w-5" />
+            <span>Rent</span>
+          </div>
+          <div className=" flex gap-2 ">
+            <input type="checkbox" id="parking" className="w-5" />
+            <span>Parking spot</span>
+          </div>
+          <div className=" flex gap-2 ">
+            <input type="checkbox" id="furnished" className="w-5" />
+            <span>Furnished</span>
+          </div>
+          <div className=" flex gap-2 ">
+            <input type="checkbox" id="offer" className="w-5" />
+            <span>Offer</span>
+          </div>
+          </div> 
         </div>
       </form>
     </main>
