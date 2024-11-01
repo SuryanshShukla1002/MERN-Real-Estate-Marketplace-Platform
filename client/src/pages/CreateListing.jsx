cfe0103ac590e8a4025c51f525d454d7fd24e533
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-11-01 15:34:55 +0530

    added input button for beds and bathroom

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -29,28 +29,49 @@ export default function CreateListing() {
             id="address"
             required
           />
-          <div className='flex gap-6 flex-wrap'>
-          <div className=" flex gap-2 ">
-            <input type="checkbox" id="sale" className="w-5" />
-            <span>Sell</span>
+          <div className="flex gap-6 flex-wrap">
+            <div className=" flex gap-2 ">
+              <input type="checkbox" id="sale" className="w-5" />
+              <span>Sell</span>
+            </div>
+            <div className=" flex gap-2 ">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className=" flex gap-2 ">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking spot</span>
+            </div>
+            <div className=" flex gap-2 ">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+            <div className=" flex gap-2 ">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
           </div>
-          <div className=" flex gap-2 ">
-            <input type="checkbox" id="rent" className="w-5" />
-            <span>Rent</span>
+          <div>
+            <div className='flex items-center gap-2'>
+              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+              <p>Beds</p>
+            </div>
+            <div className='flex items-center gap-2'>
+              <input type="number"  id="bathrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+              <p>Baths</p>
+            </div>
+            <div className='flex items-center gap-2'>
+              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+              <div>
+              <p>Regular Price</p>
+              <span>($ / Month)</span>
+              </div> 
+            </div>
+            <div className='flex items-center gap-2'>
+              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+              <p>Discounted Price( $/Month)</p>
+            </div>
           </div>
-          <div className=" flex gap-2 ">
-            <input type="checkbox" id="parking" className="w-5" />
-            <span>Parking spot</span>
-          </div>
-          <div className=" flex gap-2 ">
-            <input type="checkbox" id="furnished" className="w-5" />
-            <span>Furnished</span>
-          </div>
-          <div className=" flex gap-2 ">
-            <input type="checkbox" id="offer" className="w-5" />
-            <span>Offer</span>
-          </div>
-          </div> 
         </div>
       </form>
     </main>
