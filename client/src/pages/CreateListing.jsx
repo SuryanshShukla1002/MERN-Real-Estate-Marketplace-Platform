a9bc2c76d781eed59f9b9f47809143eb32806185
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-11-08 14:57:17 +0530

    Add image upload section with cover image note and 'Create Listing' button

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
       <h1 className="text-3xl font-semibold text-center my-7">
         Create a Listing
       </h1>
-      <form className="flex flex-col sm:flex-row">
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1 ">
           <input
             type="text"
@@ -51,27 +51,81 @@ export default function CreateListing() {
               <span>Offer</span>
             </div>
           </div>
-          <div>
-            <div className='flex items-center gap-2'>
-              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+          <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
               <p>Beds</p>
             </div>
-            <div className='flex items-center gap-2'>
-              <input type="number"  id="bathrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bathrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
               <p>Baths</p>
             </div>
-            <div className='flex items-center gap-2'>
-              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
-              <div>
-              <p>Regular Price</p>
-              <span>($ / Month)</span>
-              </div> 
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Regular Price</p>
+                <span className="text-xs">($ / Month)</span>
+              </div>
             </div>
-            <div className='flex items-center gap-2'>
-              <input type="number"  id="bedrooms" min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' />
-              <p>Discounted Price( $/Month)</p>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Discounted Price</p>
+                <span className="text-xs">($ / Month)</span>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first Image will be the cover (max 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="p-3 border border-gray-300 rounded w-full"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="p-3 text-white bg-slate-700 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create Listing
+          </button>
         </div>
       </form>
     </main>
