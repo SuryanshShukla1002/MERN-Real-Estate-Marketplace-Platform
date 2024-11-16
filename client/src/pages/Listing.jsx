852c257197856a669135234f02c45f5321723d49
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-11-16 14:38:44 +0530

    Added share button with URL copy functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
+  SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
   useEffect(() => {
     const fetchListing = async () => {
       try {
-        setLoading(loading);
+        setLoading(true);
         const res = await fetch(`/api/listing/get/${params.listingId}`);
         const data = await res.json();
         if (data.success === false) {
@@ -18,11 +25,54 @@ export default function Listing() {
           return;
         }
         setListing(data);
+        setLoading(false);
+        setError(false); // if no error now message show
       } catch (error) {
         setError(true);
+        setLoading(false);
       }
     };
     fetchListing();
-  }, []);
-  return <div>Listing</div>;
+  }, [params.listingId]);
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href); // to copy the url
+                setCopied(true);
+              }}
+            />
+          </div>
+          {copied &&
+            setTimeout(() => {
+              setCopied(false);
+            }, 2000)}
+          <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-400 p-2 text-white">
+            Link copied
+          </p>
+        </>
+      )}
+    </main>
+  );
 }
