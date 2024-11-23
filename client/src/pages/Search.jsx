061367d8c36329473c4196cd7c9ef2544a486259
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-11-23 17:50:35 +0530

    page UI starts

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,17 @@
+export default function Search() {
+  return (
+    <div>
+        <div className="">
+            <form>
+               <div className='flex items-center gap-2'>
+                <label className='whitespace-nowrap'>Search Term:</label>
+                <input type="text" id='searchTerm' placeholder='Search' className='border rounded-lg p-3 w-full' />
+               </div>
+            </form>
+        </div>
+        <div className="">
+            <h1>Listing Results</h1>
+        </div>
+    </div>
+  )
+}
