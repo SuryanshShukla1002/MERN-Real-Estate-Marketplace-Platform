7f5a08f960a1f0901f25dec73e01bcc87b9aa6f5
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-11-30 18:27:18 +0530

    Display recent offers with a 'show more' link and listings

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {Navigation} from 'swiper/modules'
-import SwiperCore from 'swiper'
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
 import "swiper/css/bundle";
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListing, setSaleListing] = useState([]);
   const [rentListings, setRentListings] = useState([]);
-  SwiperCore.use([Navigation])
+  SwiperCore.use([Navigation]);
   // console.log(saleListing);
   // console.log(offerListings);
-  
 
   useEffect(() => {
     const fetchOfferListing = async () => {
@@ -70,23 +70,33 @@ export default function Home() {
       </div>
       {/* swiper */}
       <Swiper navigation>
-        {
-          offerListings && offerListings.length > 0 && offerListings.map((listing) => (
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
             <SwiperSlide>
-              <div style={{background: `url(${listing.imageUrls[0]}) center no-repeat`, backgroundSize: "cover"}} className='h-[500px]' key={listing._id}>
-
-              </div>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[500px]"
+                key={listing._id}
+              ></div>
             </SwiperSlide>
-          ))
-        }
+          ))}
       </Swiper>
       {/* Listing results for offer, sale and rent */}
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
-          <div className=''>
+          <div className="">
             <div>
               <h2>Recent offers</h2>
-              <Link>Show more offers</Link>
+              <Link to={"/search?offer=true"}>Show more offers</Link>
+            </div>
+            <div>
+              {offerListings.map((listing) => {
+                <ListingItem listing={listing} key={listing._id} />;
+              })}
             </div>
           </div>
         )}
