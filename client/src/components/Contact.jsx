dc24549310cba9447b08a940a97a61387f98a677
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-12-22 23:58:50 +0530

    final commit

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
@@ -23,7 +24,7 @@ export default function Contact({ listing }) {
   return (
     <>
       {landlord && (
-        <div>
+        <div className="flex flex-col gap-2">
           <p>
             Contact <span className="font-semibold">{landlord.username}</span>{" "}
             for{" "}
@@ -35,7 +36,15 @@ export default function Contact({ listing }) {
             rows="2"
             value={message}
             onChange={onChanged}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded-lg"
           ></textarea>
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
         </div>
       )}
     </>
