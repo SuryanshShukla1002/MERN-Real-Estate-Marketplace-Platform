88778dc72eea773402c79255943da08543a969a5
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-12-23 23:38:23 +0530

    auth final commit

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,26 +2,26 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/User/userSlice";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
-      const auth = getAuth(app);
+      const auth = getAuth(app); //links firebase authentication to your app
 
       const result = await signInWithPopup(auth, provider);
       // console.log(result);
       // console.log(result.user.photoURL);
-      
+
       const res = await fetch("/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: 'include',
+        credentials: "include",
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
@@ -30,7 +30,7 @@ export default function OAuth() {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
-      navigate('/')
+      navigate("/");
     } catch (error) {
       console.log("could not sign in whith google", error);
     }
